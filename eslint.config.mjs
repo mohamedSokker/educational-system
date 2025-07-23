09280bdcd70e9a5000d3cabe767d037dac1742a5
mohamedSokker <mhmd.sokkar92@gmail.com>
2025-07-23 18:47:55 +0300

    full project eslint config update

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Disable unused variable warnings
+      "@typescript-eslint/no-unused-vars": "off",
+
+      // Disable 'any' type warnings
+      "@typescript-eslint/no-explicit-any": "off",
+
+      // Optional: You can disable other rules as needed
+      // "react/jsx-key": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+    },
+  },
+];
+
+export default eslintConfig;
